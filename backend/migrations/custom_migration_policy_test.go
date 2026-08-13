c51177b8f862407b37b2935f22d6ab0e84559670
Jankin_Chen <2390919165@qq.com>
2026-08-13 18:09:26 +0800

    test(migrations): allow reviewed channel monitor prefixes

--- backend/migrations/custom_migration_policy_test.go
+++ backend/migrations/custom_migration_policy_test.go
@@ -32,8 +32,10 @@ func TestLegacyCustomMigrationsRemainImmutable(t *testing.T) {
 
 func Test194And195MigrationPrefixesAreExplicitlyAllowlisted(t *testing.T) {
 	allowed := map[string]struct{}{
+		"194_channel_monitor_v2.sql":                             {},
 		"194_add_usage_log_upstream_response_model.sql":            {},
 		"194_image_generation_jobs.sql":                            {},
+		"195_channel_monitor_mode.sql":                             {},
 		"195_add_usage_log_upstream_model_mismatch_index_notx.sql": {},
 		"195_image_generation_jobs_claim_created.sql":              {},
 	}
