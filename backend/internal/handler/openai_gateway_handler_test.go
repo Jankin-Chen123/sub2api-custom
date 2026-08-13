423e6d71b0587333c5e093e06f9369ff5648532c
Jankin_Chen <2390919165@qq.com>
2026-08-13 18:38:04 +0800

    test(sync): align moderation constructor arguments

--- backend/internal/handler/openai_gateway_handler_test.go
+++ backend/internal/handler/openai_gateway_handler_test.go
@@ -1286,6 +1286,7 @@ func TestOpenAIResponsesWebSocket_ContentModerationBlocksFirstFrame(t *testing.T
 		nil,
 		nil,
 		nil,
+		nil,
 	)
 	decision, err := moderationSvc.Check(context.Background(), service.ContentModerationCheckInput{
 		UserID:   1,
